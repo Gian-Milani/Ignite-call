05158991ccf0cd250f28d4cc61957f2c9b539ac0
Gian Milani <gian.development@outlook.com>
2024-04-10 20:56:20 -0300

    Initial commit

---
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,109 @@
+import { Button, Text, TextArea, TextInput } from '@ignite-ui/react'
+import { CalendarBlank, Clock } from 'phosphor-react'
+import { ConfirmForm, FormActions, FormError, FormHeader } from './styles'
+import { z } from 'zod'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import dayjs from 'dayjs'
+import { useRouter } from 'next/router'
+import { api } from '../../../../../lib/axios'
+
+const confirmFormSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: 'O nome precisa no mínimo de 3 caracteres' }),
+  email: z.string().email({ message: 'Digite um e-mail válido' }),
+  observations: z.string().nullable(),
+})
+
+type ConfirmFormData = z.infer<typeof confirmFormSchema>
+
+interface ConfirmeStepProps {
+  schedulingDate: Date
+  onCancelConfirmation: () => void
+}
+
+export function ConfirmStep({
+  schedulingDate,
+  onCancelConfirmation,
+}: ConfirmeStepProps) {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting, errors },
+  } = useForm<ConfirmFormData>({
+    resolver: zodResolver(confirmFormSchema),
+  })
+
+  const router = useRouter()
+  const username = String(router.query.username)
+
+  async function handleConfirmScheduling(data: ConfirmFormData) {
+    const { name, email, observations } = data
+
+    await api.post(`/users/${username}/schedule`, {
+      name,
+      email,
+      observations,
+      date: schedulingDate,
+    })
+
+    onCancelConfirmation()
+  }
+
+  const describeDate = dayjs(schedulingDate).format('DD[ de ]MMMM[ de ]YYYY')
+  const describeTime = dayjs(schedulingDate).format('HH:mm[h]')
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          {describeDate}
+        </Text>
+
+        <Text>
+          <Clock />
+          {describeTime}
+        </Text>
+      </FormHeader>
+
+      <label htmlFor="">
+        <Text size="sm">Nome completo</Text>
+        <TextInput
+          placeholder="Seu nome"
+          crossOrigin="true"
+          {...register('name')}
+        />
+        {errors.name && <FormError size="sm">{errors.name.message}</FormError>}
+      </label>
+
+      <label htmlFor="">
+        <Text size="sm">Endereço de e-mail</Text>
+        <TextInput
+          type="email"
+          placeholder="johndow@example.com"
+          crossOrigin="true"
+          {...register('email')}
+        />
+        {errors.email && (
+          <FormError size="sm">{errors.email.message}</FormError>
+        )}
+      </label>
+
+      <label htmlFor="">
+        <Text size="sm">Observações</Text>
+        <TextArea {...register('observations')} />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary" onClick={onCancelConfirmation}>
+          Cancelar
+        </Button>
+        <Button type="submit" disabled={isSubmitting}>
+          Confirmar
+        </Button>
+      </FormActions>
+    </ConfirmForm>
+  )
+}
